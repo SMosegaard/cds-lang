4dac110b7c3fad546b046f1a83f6c135cc789a5c
SMosegaard <sofie.mosegaard@live.dk>
2024-03-17 11:23:15 +0100

    commit script for assignment

---
+++ assignments/assignment-3/src/scr.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import gensim.downloader as api
+
+# Load the song lyric data
+df = pd.read_csv("in/Spotify_Million_Song_Dataset_exported.csv")
+
+# Load the word embedding model via gensim
+model = api.load("glove-wiki-gigaword-50")
+
+
+"""
+First, lets define the expand_query function. The function will take a given string input (taget_word), for 
+which it will find  closely related words using a pre-trained word embedding model. 
+ 
+For example, if the target word were to be "love", the script will retrive 10 similar words (topn) such as
+"affection", "romance", etc. These words will expand the original query and allow to include a wider range of
+related words in the song lyric dataset.
+
+This is done following the documentation by gensim: https://tedboy.github.io/nlps/generated/generated/gensim.models.Word2Vec.most_similar.html 
+"""
+
+def expand_query(target_word, topn = 10):
+
+    similar_words = []
+    for word, _ in model.most_similar(target_word, topn):
+        similar_words.append(word)
+
+    return similar_words # returns a list of 10 similar words to the target word
+
+
+"""
+Now, we can define a function, that calculates the percentage of songs by the given artist that features the
+words from the expanded query. The function takes two inputs: the artist name (artist_name) and the list of words
+in the expanded query (expanded_query).
+
+The function filters the df to only include the rows where the artist column matches the input artist_name
+and finds the total number of songs by the artist. Afterwards, it will find how many songs by the artist features
+words from the expanded query. Finally, it will caculate the percentage of songs with words from expanded query.
+"""
+
+def calculate_percentage(artist_name, expanded_query):
+    artist_songs = df[df['artist'] == artist_name]['text'].str.lower()
+    total_songs = len(artist_songs)
+    songs_with_words = artist_songs.str.contains(join(expanded_query)).sum()
+    percentage = (songs_with_words / total_songs) * 100 if total_songs > 0 else 0
+    return percentage
+
+# '|'.join(expanded_query) ??
+
+def main():
+    # Enter input word for query expansion
+    target_word = input("Enter a target word: ")
+
+    # Expand the query with 10 similar words
+    expanded_query = expand_query(target_word)
+
+    # Enter input artist name
+    artist_name = input("Enter a artist's name: ")
+
+    # Calculate the percentage of an artist's songs featuring words from the expanded query
+    percentage = calculate_percentage(artist_name, expanded_query)
+
+    # Print the results
+    print(f"{percentage:.2f}% of {artist_name}'s songs contain words related to {target_word}")
+
+    # Save the results as .txt
+    filepath = f"out/{artist_name}_{target_word}_results.txt"
+    with open(filepath, 'w') as file:
+        file.write(f"{percentage:.2f}% of {artist_name}'s songs contain words related to {target_word}")
+    print("Result saved as .txt")
+
+if __name__ == "__main__":
+    main()
