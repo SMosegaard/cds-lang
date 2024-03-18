cca32cd2e1c4d38db9eb02d5ecf39884c8579bb8
SMosegaard <sofie.mosegaard@live.dk>
2024-03-18 13:49:07 +0100

    commit changes assignment 3

--- assignments/assignment-3/src/scr.py
+++ assignments/assignment-3/src/scr.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import os
 import gensim.downloader as api
+import argparse
 
 # Load the song lyric data
 df = pd.read_csv("in/Spotify_Million_Song_Dataset_exported.csv")
@@ -7,52 +9,47 @@ df = pd.read_csv("in/Spotify_Million_Song_Dataset_exported.csv")
 # Load the word embedding model via gensim
 model = api.load("glove-wiki-gigaword-50")
 
+def expand_query(model, target_word, topn = 10):
 
-"""
-First, lets define the expand_query function. The function will take a given string input (taget_word), for 
-which it will find  closely related words using a pre-trained word embedding model. 
- 
-For example, if the target word were to be "love", the script will retrive 10 similar words (topn) such as
-"affection", "romance", etc. These words will expand the original query and allow to include a wider range of
-related words in the song lyric dataset.
-
-This is done following the documentation by gensim: https://tedboy.github.io/nlps/generated/generated/gensim.models.Word2Vec.most_similar.html 
-"""
-
-def expand_query(target_word, topn = 10):
-
-    similar_words = []
-    for word, _ in model.most_similar(target_word, topn):
-        similar_words.append(word)
-
+    """
+    First, lets define the expand_query function. The function will take a given string input (taget_word), for 
+    which it will find  closely related words using a pre-trained word embedding model. 
+    
+    For example, if the target word were to be "love", the script will retrive 10 similar words (topn) such as
+    "affection", "romance", etc. These words will expand the original query and allow to include a wider range of
+    related words in the song lyric dataset.
+    """
+    
+    similar_words = [word for word, _ in model.most_similar(target_word, topn=topn)]
     return similar_words # returns a list of 10 similar words to the target word
 
 
-"""
-Now, we can define a function, that calculates the percentage of songs by the given artist that features the
-words from the expanded query. The function takes two inputs: the artist name (artist_name) and the list of words
-in the expanded query (expanded_query).
-
-The function filters the df to only include the rows where the artist column matches the input artist_name
-and finds the total number of songs by the artist. Afterwards, it will find how many songs by the artist features
-words from the expanded query. Finally, it will caculate the percentage of songs with words from expanded query.
-"""
-
 def calculate_percentage(artist_name, expanded_query):
+    
+    """
+    Now, we can define a function, that calculates the percentage of songs by the given artist that features the
+    words from the expanded query. The function takes two inputs: the artist name (artist_name) and the list of words
+    in the expanded query (expanded_query).
+
+    The function filters the df to only include the rows where the artist column matches the input artist_name
+    and finds the total number of songs by the artist. Afterwards, it will find how many songs by the artist features
+    words from the expanded query. Finally, it will caculate the percentage of songs with words from expanded query.
+    """
+
     artist_songs = df[df['artist'] == artist_name]['text'].str.lower()
     total_songs = len(artist_songs)
-    songs_with_words = artist_songs.str.contains(join(expanded_query)).sum()
+    songs_with_words = artist_songs.str.contains('|'.join(expanded_query)).sum()
     percentage = (songs_with_words / total_songs) * 100 if total_songs > 0 else 0
     return percentage
 
-# '|'.join(expanded_query) ??
 
 def main():
+
     # Enter input word for query expansion
     target_word = input("Enter a target word: ")
 
     # Expand the query with 10 similar words
-    expanded_query = expand_query(target_word)
+    expanded_query = expand_query(model, target_word)
 
     # Enter input artist name
     artist_name = input("Enter a artist's name: ")
