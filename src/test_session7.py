beaa1343a6cb0db673f9326140f9575a2b76ada8
SMosegaard <sofie.mosegaard@live.dk>
2024-03-21 11:57:22 +0100

    add week 7 script

---
+++ src/test_session7.py
@@ -0,0 +1,8 @@
+def main():
+    i = 0
+    while i < 100: # While True = print infinitely (until we interrup it by pressing Ctrl+C)
+        print(i)
+        i += 1
+
+if __name__=="__main__":
+    main()
