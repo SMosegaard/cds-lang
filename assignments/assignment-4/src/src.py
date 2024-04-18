e3a88692f2846290ee313c26fe880e21bb02671a
SMosegaard <sofie.mosegaard@live.dk>
2024-04-18 17:28:14 +0200

    added comments in the code

--- assignments/assignment-4/src/src.py
+++ assignments/assignment-4/src/src.py
@@ -1,22 +1,14 @@
 import pandas as pd
-import matplotlib
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import seaborn as sns
 from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer
 
-# Dictionary for colors mapped to emotions
-emotion_colors = {
-                "disgust": "purple",
-                "surprise": "yellow",
-                "neutral": "lightgray",
-                "fear": "orange",
-                "anger": "red",
-                "joy": "green",
-                "sadness": "blue"}
-
 
 def load_classifier():
+    """
+    Load pre-trained text classification model from HuggingFace
+    """
     classifier = pipeline("text-classification", 
                       model = "j-hartmann/emotion-english-distilroberta-base", 
                       return_all_scores = False)
@@ -24,25 +16,36 @@ def load_classifier():
 
 
 def predict_emotion(df, classifier):
+    """
+    Predict emotions for each sentence in the dataframe
+    """
     for index, row in df.iterrows():
         if type(row["Sentence"]) == str:
             predicted_emotion = classifier(row["Sentence"])[0]['label']
             df.at[index, "predicted_emotion"] = predicted_emotion
     return df
 
-def plot_season(df, emotion_colors, outpath):
-    total_counts = df.groupby('Season')['predicted_emotion'].value_counts(normalize = True) * 100
-    plot = sns.catplot(data = df, x = "predicted_emotion", hue = "predicted_emotion", col = "Season", kind = "count", 
-                palette = emotion_colors.values() , col_wrap = 4) #lengend = False
+
+def plot_season(df, outpath):
+    """
+    Plot the distribution of predicted emotions across seasons
+    """
+    real_freq = df.groupby('Season')['predicted_emotion'].value_counts(normalize = True) * 100
+    plot = sns.catplot(data = df, x = "predicted_emotion", hue = "predicted_emotion", col = "Season", kind = "bar", 
+                palette = "husl", legend = False)
     plot.set_axis_labels("", "Relative Frequency (%)")
     plot.set_titles("{col_name}")
     plt.savefig(outpath)
     return print("The 'season' plot has been saved to the out folder")
 
+
 def plot_emotion(df, outpath):
-    total_counts = df.groupby('predicted_emotion')['Season'].value_counts(normalize = True) * 100
-    plot = sns.catplot(data = df, x = "Season", hue = "Season", col = "predicted_emotion", kind = "count",
-                    palette = "husl", col_wrap = 4)
+    """
+    Plot the relative frequency of each emotion across all seasons
+    """
+    real_freq = df.groupby('predicted_emotion')['Season'].value_counts(normalize = True) * 100
+    plot = sns.catplot(data = df, x = "Season", hue = "Season", col = "predicted_emotion", kind = "bar",
+                    palette = "husl")
     plot.set_axis_labels("", "Relative Frequency (%)")
     plot.set_titles("{col_name}")
     plt.savefig(outpath)
@@ -50,27 +53,21 @@ def plot_emotion(df, outpath):
 
 
 def save_df_to_csv(df, csv_outpath):
+    """
+    Save the dataframe with predicted emotions as .csv
+    """
     df.to_csv(csv_outpath)
     return print("The dataframe with the predicted emotion has been saved to the out folder")
 
 
 def main():
 
-    df = pd.read_csv("../../../../cds-lang-data/GoT-scripts/Game_of_Thrones_Script.csv")
-    
+    df = pd.read_csv("in/Game_of_Thrones_Script.csv")
     classifier = load_classifier()
-
-    df = df.head(10)
-
     df = predict_emotion(df, classifier)
-    print(df)
-
     save_df_to_csv(df, "out/data.csv")
-
-    plot_season(df, emotion_colors, "out/season.png")
-
-    plot_emotion(df, "out/emotion.png")
+    plot_season(df, "out/season2.png")
+    plot_emotion(df, "out/emotion2.png")
 
 if __name__ == "__main__":
     main()
-
