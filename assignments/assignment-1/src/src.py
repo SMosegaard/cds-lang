25b3abad00a1025a5e47adfeab3f4f2b70c1347e
SMosegaard <sofie.mosegaard@live.dk>
2024-05-02 10:19:31 +0200

    updated, changed process_text() due to error in save df

--- assignments/assignment-1/src/src.py
+++ assignments/assignment-1/src/src.py
@@ -3,12 +3,23 @@ import pandas as pd
 import glob
 import re
 import spacy
+from codecarbon import EmissionsTracker
+
+def emissions_tracker(tracker_outpath):
+    tracker = EmissionsTracker(project_name = "assignment 1",
+                                output_dir = tracker_outpath,
+                                output_file = "emissions_assignment1.csv")
+    return tracker
 
 
-nlp = spacy.load("en_core_web_md")
+def load_spacy():
+    """ Load SpaCy model """
+    nlp = spacy.load("en_core_web_md")
+    return nlp
 
 
 def remove_metadata(text):
+    """ Remove metadata from text input """
     return re.sub(r"<*?>", "", text)
 
 
@@ -67,24 +78,21 @@ def no_unique_ents(doc):
     return unique_row
 
 
-
-def process_text(filepath):
-
+def process_text(filepath, nlp):
     """ 
     The function iterates over .txt files in the subfolders and extracts linguistic features. Also, it creates a
     Pandas DataFrame to store and append the extracted features for each file.
     """
-
     for subfolder in sorted(os.listdir(filepath)):
         subfolder_path = os.path.join(filepath, subfolder)
 
-        out_df = pd.DataFrame(columns=("Filename", "RelFreq NOUN", "RelFreq VERB", "RelFreq ADV",
+        out_df = pd.DataFrame(columns = ("Filename", "RelFreq NOUN", "RelFreq VERB", "RelFreq ADV",
                                         "RelFreq ADJ", "No. Unique PER", "No. Unique LOC", "No. Unique ORG"))
 
         csv_outpath = os.path.join("out", f"{subfolder}.csv")
 
-        for file in glob.glob(os.path.join(subfolder_path, "*.txt")):
-            with open(file, "r", encoding="latin-1") as f:
+        for file in sorted(glob.glob(os.path.join(subfolder_path, "*.txt"))):
+            with open(file, "r", encoding = "latin-1") as f:
                 text = f.read()
                 doc = nlp(remove_metadata(text))
 
@@ -92,26 +100,30 @@ def process_text(filepath):
             len_doc = len(doc)
             noun_rel_freq, verb_rel_freq, adv_rel_freq, adj_rel_freq = rel_freq(noun_count, len_doc), rel_freq(verb_count, len_doc), rel_freq(adv_count, len_doc), rel_freq(adj_count, len_doc)
             No_unique_per, No_unique_loc, No_unique_org = no_unique_ents(doc)
-            text_name = text.split("/")[-1]
+            
+            text_name = file.split("/")[-1]
+
             text_row = [text_name, noun_rel_freq, verb_rel_freq, adv_rel_freq, adj_rel_freq,
                         No_unique_per, No_unique_loc, No_unique_org]
             out_df.loc[len(out_df)] = text_row
-        
-        #print(out_df)
-        
 
-def save_dataframe_to_csv(df, csv_outpath):
-    df.to_csv(csv_outpath)
+        csv_outpath = os.path.join("out", f"{subfolder}_data.csv")
+        out_df.to_csv(csv_outpath)
 
 
 def main():
-
-    filepath = os.path.join("in", "USEcorpus")
-    results = process_text(filepath)
     
-    csv_outpath = os.path.join("out", f"{subfolder}_data_new.csv")
-    save_dataframe_to_csv(results, csv_outpath)
+    tracker_outpath = "../assignment-5/out"
+    tracker = emissions_tracker(tracker_outpath)
+    tracker.start()
 
+    nlp = load_spacy()
+
+    filepath = os.path.join("..", "..", "..", "..", "cds-lang-data", "USEcorpus", "USEcorpus") # "in", "USEcorpus"
+
+    results = process_text(filepath, nlp)
+
+    tracker.stop()
 
 if __name__ == "__main__":
     main()
