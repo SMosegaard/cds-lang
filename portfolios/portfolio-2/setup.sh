3b2f57685be977e3ddfd60f27c294ceeec6bfec7
SMosegaard <sofie.mosegaard@live.dk>
2024-05-21 13:12:00 +0200

    updated

--- portfolios/portfolio-2/setup.sh
+++ portfolios/portfolio-2/setup.sh
@@ -1,12 +1,12 @@
-# Create virtual envoriment called env
+# Create a virtual envoriment called env
 python -m venv env
 
-# Activate the virtual env
-source ./env/bin/activate   
+# Activate the virtual envoriment
+source ./env/bin/activate
 
 # Install requirements
 pip install --upgrade pip
-pip install -r requirements.txt 
+pip install -r requirements.txt
 
-# Close the envoriment
-deactivate
+# Close the virtual envoriment
+#deactivate
