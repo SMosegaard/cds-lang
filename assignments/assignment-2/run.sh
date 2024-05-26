ffcc1195df182efbb3a9beeb29e698d9f0f5e4db
SMosegaard <sofie.mosegaard@live.dk>
2024-05-26 16:17:17 +0200

    added comment

--- assignments/assignment-2/run.sh
+++ assignments/assignment-2/run.sh
@@ -4,10 +4,10 @@ source ./env/bin/activate
 # Run the vectorizer
 python src/vectorizer.py
 
-# Run the LR classifier
+# Run the LR classifier with argparse arguments
 python src/LR_classifier.py "$@"
 
-# Run the NN classifier
+# Run the NN classifier with argparse arguments
 python src/NN_classifier.py "$@"
 
 # Close the virtual envoriment
